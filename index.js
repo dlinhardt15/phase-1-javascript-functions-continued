53b636e31baacc195ff3218f1b01dc2347795c14
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-17 13:54:37 -0400

    commit tree 1a703da501f549986f715c341bd7492c29db055d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`; 
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(visual = "*") {
+    return function phrase(descriptor = "special") {
+        return `You are ${visual}${descriptor}${visual}!`;
+    };
+}
+wrapAdjective("%")("a dedicated programmer");
